b5bf3d3c41861350d84e337034ff6879ad230209
Yvan Janssens <ik@yvanj.me>
2012-12-31 20:00:42 +0100

    First draft of Connection and Via models

---
+++ NMBS/Connection.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+
+namespace NetRail.NMBS
+{
+	public class Connection
+	{
+		/// <summary>
+		/// Gets or sets the departure station.
+		/// </summary>
+		/// <value>
+		/// The departure station.
+		/// </value>
+		public Station DepartureStation { get; set; }
+
+		/// <summary>
+		/// Gets or sets the arrival station.
+		/// </summary>
+		/// <value>
+		/// The arrival station.
+		/// </value>
+		public Station ArrivalStation { get; set; }
+
+		/// <summary>
+		/// Gets or sets the time of departure
+		/// </summary>
+		/// <value>
+		/// The time departure.
+		/// </value>
+		public DateTime DepartureTime { get; set; }
+
+		/// <summary>
+		/// Gets or sets the time of arrival
+		/// </summary>
+		/// <value>
+		/// The time of arrival.
+		/// </value>
+		public DateTime ArrivalTime { get; set; }
+
+		/// <summary>
+		/// Gets or sets the platform of departure
+		/// </summary>
+		/// <value>
+		/// The platform of departure.
+		/// </value>
+		public string DeparturePlatform { get; set; }
+
+		/// <summary>
+		/// Gets or sets the platform of arrival.
+		/// </summary>
+		/// <value>
+		/// The platform of arrival.
+		/// </value>
+		public string ArrivalPlatform { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="NetRail.NMBS.Connection"/> departure platform changed.
+		/// </summary>
+		/// <value>
+		/// <c>true</c> if departure platform changed; otherwise, <c>false</c>.
+		/// </value>
+		public bool DeparturePlatformChanged { get; set; }
+
+		/// <summary>
+		/// Gets or sets a value indicating whether this <see cref="NetRail.NMBS.Connection"/> arrival platform changed.
+		/// </summary>
+		/// <value>
+		/// <c>true</c> if arrival platform changed; otherwise, <c>false</c>.
+		/// </value>
+		public bool ArrivalPlatformChanged { get; set; }
+
+		/// <summary>
+		/// Gets or sets the duration.
+		/// </summary>
+		/// <value>
+		/// The duration.
+		/// </value>
+		public int Duration { get; set; }
+
+		/// <summary>
+		/// Gets or sets the vias.
+		/// </summary>
+		/// <value>
+		/// The vias.
+		/// </value>
+		public List<Via> Vias { get; set; }
+
+		public Connection ()
+		{
+		}
+	}
+}
+
---
+++ NMBS/Via.cs
@@ -0,0 +1,64 @@
+using System;
+
+namespace NetRail.NMBS
+{
+	public class Via
+	{
+		/// <summary>
+		/// Gets or sets the arrival time.
+		/// </summary>
+		/// <value>
+		/// The arrival time.
+		/// </value>
+		public DateTime ArrivalTime { get; set; }
+
+		/// <summary>
+		/// Gets or sets the departure time.
+		/// </summary>
+		/// <value>
+		/// The departure time.
+		/// </value>
+		public DateTime DepartureTime { get; set; }
+
+		/// <summary>
+		/// Gets or sets the time between transits.
+		/// </summary>
+		/// <value>
+		/// The time between the transit.
+		/// </value>
+		public int TimeBetween { get; set; }
+
+
+		/// <summary>
+		/// Gets or sets the vehicle used.
+		/// </summary>
+		/// <value>
+		/// The vehicle used.
+		/// </value>
+		public Vehicle VehicleUsed {get;set;}
+
+		/// <summary>
+		/// Gets or sets the arrival station.
+		/// This is the station in which you make the transit to another train.
+		/// </summary>
+		/// <value>
+		/// The arrival station.
+		/// </value>
+		public Station ArrivalStation { get; set; }
+
+		/// <summary>
+		/// Gets or sets the destination station.
+		/// This is the station to where the train is heading to after you performed
+		/// the transit.
+		/// </summary>
+		/// <value>
+		/// The destination station.
+		/// </value>
+		public Station DestinationStation { get; set; }
+
+		public Via ()
+		{
+		}
+	}
+}
+
