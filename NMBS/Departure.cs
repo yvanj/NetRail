791d9efa828b66ac1a97007be5ce463f03e13c5d
Yvan Janssens <ik@yvanj.me>
2012-12-31 13:52:47 +0100

    Initial commit.
    Departure and Station class modeled.

---
+++ NMBS/Departure.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetRail.NMBS
+{
+    /// <summary>
+    /// The data structure containing departure information
+    /// </summary>
+    class Departure
+    {
+        /// <summary>
+        /// The time the train departs.
+        /// </summary>
+        public DateTime Time { get; set; }
+        /// <summary>
+        /// The delay (if applicable) of the train
+        /// </summary>
+        public int Delay { get; set; }
+        /// <summary>
+        /// The termination point of the station
+        /// </summary>
+        public string Direction { get; set; }
+        /// <summary>
+        /// The ID of the vehicle of the train
+        /// </summary>
+        public string Vehicle { get; set; }
+        /// <summary>
+        /// The number of the platform of the train
+        /// </summary>
+        public string Platform { get; set; }
+
+        /// <summary>
+        /// Returns true if the platform specified in the data is not the planned platform
+        /// </summary>
+        public bool PlatformChanged { get; set; }
+    }
+}
---
+++ NMBS/Station.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetRail.NMBS
+{
+    /// <summary>
+    /// The data structure containing station information.
+    /// </summary>
+    class Station
+    {
+        /// <summary>
+        /// The ID of the station
+        /// </summary>
+        string Id { get; set; }
+        /// <summary>
+        /// The name of the station
+        /// </summary>
+        string Name { get; set; }
+        /// <summary>
+        /// The longitude of the station
+        /// </summary>
+        float Longitude { get; set; }
+        /// <summary>
+        /// The latitude of the station
+        /// </summary>
+        float Latitude { get; set; }
+
+        /// <summary>
+        /// A list of the current departures in the station
+        /// </summary>
+        public List<Departure> Departures
+        {
+            
+            get
+            {
+                // TODO: make the Station fetch the Departures.
+                return null;
+            }
+            set
+            {
+
+
+            }
+        }
+    }
+}
