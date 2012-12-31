6b533644640f2964e049a66a0c198540e74cd4c5
Yvan Janssens <ik@yvanj.me>
2012-12-31 14:07:37 +0100

    Stop and Vehicle classes modeled further.

---
+++ NMBS/Stop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetRail.NMBS
+{
+    /// <summary>
+    /// The model for a stop from a vehicle.
+    /// </summary>
+    class Stop
+    {
+        /// <summary>
+        /// The time the vehicle stops in the station
+        /// </summary>
+        public DateTime Time { get; set; }
+        /// <summary>
+        /// The station in which the vehicle stops
+        /// </summary>
+        public Station Station { get; set; }
+    }
+}
---
+++ NMBS/Vehicle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetRail.NMBS
+{
+    /// <summary>
+    /// The data structure containing vehicle information
+    /// </summary>
+    class Vehicle
+    {
+        /// <summary>
+        /// The ID of the vehicle
+        /// </summary>
+        public string Id { get; set; }
+
+        
+        /// <summary>
+        /// The list of stations in which this train stops.
+        /// </summary>
+        public List<Stop> Stops
+        {
+            get
+            {
+                // TODO: implement stops fetching.
+                return null;
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
